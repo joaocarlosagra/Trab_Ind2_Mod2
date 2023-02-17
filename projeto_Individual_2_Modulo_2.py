13d48576c95df0b0d26a6ff9af9b42feefa8f12b
joaocarlosagra <117124630+joaocarlosagra@users.noreply.github.com>
2023-02-17 01:39:57 -0300

    Update projeto_Individual_2_Modulo_2.py

--- projeto_Individual_2_Modulo_2.py
+++ projeto_Individual_2_Modulo_2.py
@@ -6,33 +6,31 @@ def cad():
     sair = "n"
     while sair != "s":
         #cand = dict()
-        nome = input("Digite seu nome: ")
-        carta = input("Digite sua apresentação: ")
-        cand: dict(nome, carta) = dict()
-        cand.update()
-    sair = input("Deseja sair? s ou n: ")
+        nome = input("Digite seu nome: ").lower()
+        carta = input("Digite sua apresentação: ").lower()
+        #cand: dict(nome, carta) = dict()
+        cand[nome] = (carta)
+        sair = input("Deseja sair? s ou n: ")
 
 
 def imp():
     print("-"*50)
-    #print(f"O Candidato {Nome} tem a caracteristica {Carta}.")
+    #print(f"O Candidato {cad.keys()} tem a caracteristica {cad.values()}.")
     print(f"{cand}")
     print("-" * 50)
 
+def busca():
+    #vaga1 = ("python" and "desenvolvedor" and "programador")
+    #vaga2 = ("analise de dados" and "dados" and "sql")
+    for palavra in cand.items():
+        if "python" or "desenvolvedor" or "programador" in palavra:
+            print(f"{palavra} atende vaga1")
+        elif "analise de dados" or "dados" or "sql" in palavra:
+            print(f"{palavra} atende vaga2")
+        else:
+            print(f"{palavra} não atende as vagas.")
 
 
 cad()
 imp()
-print("-"*50)
-
-vaga1 = ("python" + "desenvolvedor" + "programador")
-vaga2 = ("analise de dados" + "dados" + "sql")
-for palavra in cand.values():
-    if vaga1 in palavra:
-        print(f"vaga1: {palavra}")
-    elif vaga2 in palavra:
-        print(f"vaga2: {palavra}")
-    else:
-        print(f"não atende: {palavra}")
-
-
+busca()
